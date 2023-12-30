f345906e8706639be858cc5dc991e7ed9e7f7ecd
ashwani <ashnit8294@gmail.com>
2023-12-30 17:25:13 +0530

    added productList

---
+++ react-ecommerce/src/features/productList/ProductList.js
@@ -0,0 +1,5 @@
+
+const ProductList = ()=>{
+ return <div>hello</div>
+}
+export default ProductList;
---
+++ react-ecommerce/src/features/productList/productListSlice.js
@@ -0,0 +1,6 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    list:0
+}
+
