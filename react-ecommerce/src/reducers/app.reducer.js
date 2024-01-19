da7926dcebeea23ff64b8ba4408cd75a6f49a81b
ashwani <ashnit8294@gmail.com>
2024-01-19 19:10:09 +0530

    cors error

--- react-ecommerce/src/reducers/app.reducer.js
+++ react-ecommerce/src/reducers/app.reducer.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 const initURL = "https://apiecommerce-bz4f.onrender.com";
 
 const initialState = {
